a0835eb782006f5d7b5ba59a2c7f1619c9d36373
PEDRO HENRIQUE PINHO BASTOS <pedro_hp_bastos@estudante.sc.senai.br>
2024-10-15 20:19:34 -0300

    dto arrumado

---
+++ src/main/java/com/example/demo/entities/DTO/CriarEstoqueDTO.java
@@ -0,0 +1,26 @@
+package com.example.demo.entities.DTO;
+
+public class CriarEstoqueDTO {
+
+
+        private String descricao;
+        private String status;
+
+
+
+        public String getDescricao() {
+            return descricao;
+        }
+        public void setDescricao(String descricao) {
+            this.descricao = descricao;
+        }
+        public String getStatus() {
+            return status;
+        }
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+
+    }
+
